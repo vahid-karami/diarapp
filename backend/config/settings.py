b185d2eb1041dd8e38e77705c59e298795d7d3aa
Vahid Karami <vahidkarami1380@gmail.com>
2026-08-11 20:59:43 +0330

    added image upload.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -112,8 +112,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+# NEW: Media files (Uploaded by users like Receipts)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
 ]
+
+# This allows Django to serve uploaded images (receipts) during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
