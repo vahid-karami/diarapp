6baec5906edbbaabed28fa055e6bb6e5d00f1ca9
Vahid Karami <vahidkarami1380@gmail.com>
2026-08-11 16:59:48 +0330

    Configure custom user model, update compose command, and apply initial migrations

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# Register our CustomUser using Django's secure UserAdmin interface
+admin.site.register(CustomUser, UserAdmin)
+
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # We are inheriting all default fields (username, email, password, etc.)
+    # Future expansion (e.g., roles, phone numbers) can be added here easily.
+    
+    def __str__(self):
+        return self.username
